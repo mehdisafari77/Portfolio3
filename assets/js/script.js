5f150cdbefff0d13a2ca31112bb2935b5437449f
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-11-07 10:13:57 -0800

    add navbar

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('#button');
+const tooltip = document.querySelector('#tooltip');
+
+// Pass the button, the tooltip, and some options, and Popper will do the
+// magic positioning for you:
+Popper.createPopper(button, tooltip, {
+  placement: 'right',
+});
